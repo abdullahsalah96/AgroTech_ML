62af1a94900d98a3041299905208648d38e2e6ec
abdullahsalah96 <abdullahsalah96@outlook.com>
2019-10-23 23:42:23 +0200

    modified training

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from sklearn.datasets import load_files
 from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers import Dense, MaxPooling2D, GlobalAveragePooling2D, Conv2D, Dropout, Flatten
-from keras.models import model_from_json
+from keras.models import model_from_json, load_model
 from keras.optimizers import SGD, RMSprop
 from keras.callbacks import ModelCheckpoint
 from keras.preprocessing import image
@@ -48,35 +48,6 @@ test_images, ts_labels = imageLoader.load_images(r"/home/abdullahsalah96/IBM/Dat
 train_tensors = imageLoader.paths_to_tensor(train_images, (32,32)).astype('float32')
 test_tensors = imageLoader.paths_to_tensor(test_images, (32,32)).astype('float32')
 
-# ann = Annotations()
-# #getting training labels
-# training_annotations = ann.getAnnotationsDataframe(r"path to training annotations")
-# resized_training_annotations = ann.resizeBoundingBoxes(training_annotations, (32,32))
-# training_labels = ann.getLabels(resized_training_annotations)
-# training_bounding_boxes = ann.getBoundingBoxes(resized_training_annotations)
-# training_encoded_bounding_boxes = ann.oneHotEncode(training_bounding_boxes, (32,32))
-# final_training_bounding_boxes = training_encoded_bounding_boxes.reshape([39209, 128]) #reshape bounding boxes where each 32 elements is a new point
-#
-# #getting testing labels
-# testing_annotations = ann.getAnnotationsDataframe(r"path to testing annotations")
-# resized_testing_annotations = ann.resizeBoundingBoxes(testing_annotations, (32,32))
-# testing_labels = ann.getLabels(resized_testing_annotations)
-# testing_bounding_boxes = ann.getTestingBoundingBoxes(resized_testing_annotations)
-#
-## concatenating bbxes with CLASSES
-# print(train_images[0])
-# print("--FINAL CLASSES-- \n")
-# print(tr_labels[0])
-# print("--FINAL BOXES-- \n")
-# print(training_bounding_boxes[0])
-# print("--FINAL ENCODED BOUDNING BOXES-- \n")
-# print(training_encoded_bounding_boxes[0])
-# print("--FINAL FLATTENED BOXES-- \n")
-# print(final_training_bounding_boxes[0])
-#
-# print("--FINAL TESTING BOXES")
-# print(testing_bounding_boxes)
-
 ##########################  [SOIL SOYBEANS WEEDS]   ############################
 # print(train_images[1500])
 # print(tr_labels[1500])
@@ -96,7 +67,7 @@ classification_model.summary()
 #compiling the classification_model
 classification_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 #making a checkpointer
-checkpointer = ModelCheckpoint(filepath = r'best_weights\final.hdf5', verbose = 1, save_best_only = True)
+checkpointer = ModelCheckpoint(filepath = r'final.hdf5', verbose = 1, save_best_only = True)
 
 #fitting the model
 classification_model.fit(train_tensors, tr_labels, batch_size = 100, nb_epoch = 20, validation_split=0.2, callbacks=[checkpointer], shuffle = True)
@@ -104,6 +75,8 @@ classification_model.fit(train_tensors, tr_labels, batch_size = 100, nb_epoch = 
 # saving the classification_model
 classification_model.save("model.h5")
 
-# score = classification_model.evaluate(test_images, ts_labels, verbose = 1)
-# accuracy = score[1] *100 #score[0] returns loss value, score[1] returns the metrics value (accuracy)
-# print(r'\n\ test loss: ', accuracy)
+
+classification_model = load_model('model.h5')
+score = classification_model.evaluate(test_tensors, ts_labels, verbose = 1)
+accuracy = score[1] *100 #score[0] returns loss value, score[1] returns the metrics value (accuracy)
+print(r'\n\ test accuracy: ', accuracy)
